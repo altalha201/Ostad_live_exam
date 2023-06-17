39f92a622103e585291e9b0cbcd0bfcc39474932
Md. Motasaddique Al Talha <67387060+altalha201@users.noreply.github.com>
2023-06-18 00:08:45 +0600

    SS add

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Pagination Example',
+      title: 'Post Live',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
